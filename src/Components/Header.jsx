7982992180ca3bc0b1d15aaa03c22beb4304ca77
Sahil3982 <sahilpatel3982@gmail.com>
2024-05-23 09:12:58 +0530

    Trying to implement navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,12 +4,21 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <nav>
-        <div>
-        <Link to='/' > Logo</Link>
-        </div>
-        <div>
+      <div className='relative flex gap-4 justify-between items-center mb-2'>
+        <Link to='/' className='flex gap-1' >
+          <img src='/logo.png' className='w-8' alt='Giphy Logo'></img>
+          <h1 className='text-5xl font-extrabold tracking-tight cursor-pointer'>GIPHY</h1>
+        </Link>
+        <Link className='px-4 py-1 hover:gradient border-b-4 hidden lg:block'>
+          Reactions
+        </Link>
+        <button>
 
-        </div>
+        </button>
+      </div>
+      <div>
+       
+      </div>
     </nav>
   )
 }
--- src/Layouts/AppLayout.jsx
+++ src/Layouts/AppLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../Components/Header'
 
 const AppLayout = () => {
   return (
     <div className='bg-gray-900 text-white min-h-screen'>
-      {/* Header */}
+      <Header />
 
       <main>
         <Outlet />
