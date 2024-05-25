82106ca05a2daeca4d67f20732d4edac0ec6424b
Sahil3982 <sahilpatel3982@gmail.com>
2024-05-25 14:53:19 +0530

    Created gif page

--- src/Components/Gif.jsx
+++ src/Components/Gif.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Gif = () => {
+const Gif = ({gif , hover = true}) => {
   return (
-    <div>Gif</div>
+    <Link to={`${gif.type}/${gif.slug}`}>
+      <div className='w-full mb-2 relative cursor-pointer'>
+        <img 
+        src={gif?.images?.fixed_width.webp}
+        alt={gif?.title}
+        className='w-full object-cover rounded transition-all duration-300'
+        />
+        {
+          hover && <div className='absolute inset-0 rounded opacity-0 group-hover:opacity-100'>
+          <img src={gif?.user?.avatar_url}
+          alt={gif?.user?.display_name}
+          className='h-8'
+          />
+          <span>{gif?.user?.display_name}</span>
+          </div>
+        }
+      </div>
+    </Link>
   )
 }
 
